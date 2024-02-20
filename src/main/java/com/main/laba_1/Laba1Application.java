9bc20e67d20c6b846605c00c34c9f3a6d45749bf
rudemaru <ruslandemidovich2005@gmail.com>
2024-02-20 20:00:34 +0300

    stable version

--- src/main/java/com/main/laba_1/Laba1Application.java
+++ src/main/java/com/main/laba_1/Laba1Application.java
@@ -1,6 +1,5 @@
 package com.main.laba_1;
 
-import com.main.laba_1.model.GroupSchedule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -12,19 +11,5 @@ public class Laba1Application {
 	public static void main(String[] args) {
 
 		SpringApplication.run(Laba1Application.class, args);
-
-		String url = "https://iis.bsuir.by/api/v1/schedule?studentGroup=250503";
-		WebClient.Builder builder = WebClient.builder();
-
-		String groupSchedule = builder.build()
-				.get()
-				.uri(url)
-				.retrieve()
-				.bodyToMono(String.class)
-				.block();
-
-		System.out.println("------------------------------------------------------");
-		System.out.println(groupSchedule);
-		System.out.println("------------------------------------------------------");
 	}
 }
--- src/main/java/com/main/laba_1/controllers/MainController.java
+++ src/main/java/com/main/laba_1/controllers/MainController.java
@@ -1,10 +1,31 @@
 package com.main.laba_1.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.logging.Logger;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
-@RequestMapping("/api/v1/")
 public class MainController {
-    
+    Logger logger = Logger.getLogger(getClass().getName());
+    @GetMapping("/getsched")
+
+    public void getSched(@RequestParam(value = "groupNumber", defaultValue = "250503") String groupNumber) {
+        String template = "https://iis.bsuir.by/api/v1/schedule?studentGroup=%s";
+
+        String url = String.format(template, groupNumber);
+        WebClient.Builder builder = WebClient.builder();
+
+        String groupSchedule = builder.build()
+                .get()
+                .uri(url)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        logger.info("------------------------------------------------------");
+        logger.info(groupSchedule);
+        logger.info("------------------------------------------------------");
+    }
 }
